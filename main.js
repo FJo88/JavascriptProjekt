6175405759c7660864fa6108865dc1c1af0c9134
Filip Johnsson <filip.j.datatech@gmail.com>
2021-12-06 22:12:02 +0100

    Påbörjar Vg

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ let studentList = document.querySelector("#leftList");
 let outputList = document.querySelector("#rightList")
 let sortEdu = document.querySelector("#sortE");
 let getEdu = document.querySelector("#getEdu");
-let sortAgeBtn = document.querySelector("#sortA");
-let sortFirstBtn = document.querySelector("#sortFirst");
-let sortLastBtn = document.querySelector("#sortLast");
-let green = false;
-let yellow = false;
-let red = false;
+let sortAgeUpBtn = document.querySelector("#sortAu");
+let sortAgeDownBtn = document.querySelector("#sortAd");
+let sortFirstUpBtn = document.querySelector("#sortFirstu");
+let sortFirstDownBtn = document.querySelector("#sortFirstd"); 
+let sortLastUpBtn = document.querySelector("#sortLastu");
+let sortLastDownBtn = document.querySelector("#sortLastd");
+
 document.body.style.background = "lightblue";
 
 
@@ -25,19 +26,34 @@ getEdu.addEventListener(("click"), () => {
   sortByEducation();
 });
 
-sortAgeBtn.addEventListener(("click"), ()=> {
+sortAgeUpBtn.addEventListener(("click"), ()=> {
     outputList.textContent = "";
-    sortByAge();
+    sortByAgeUp();
 });
 
-sortFirstBtn.addEventListener(("click"), ()=> {
-    outputList.textContent = "";
-    sortByFirstName();
+sortAgeDownBtn.addEventListener(("click"), ()=> {
+  outputList.textContent = "";
+  sortByAgeDown();
 });
 
-sortLastBtn.addEventListener(("click"), ()=> {
+
+sortFirstUpBtn.addEventListener(("click"), ()=> {
     outputList.textContent = "";
-    sortByLastName();
+    sortByFirstNameUp();
+});
+
+sortFirstDownBtn.addEventListener(("click"), ()=> {
+  outputList.textContent = "";
+  sortByFirstNameDown();
+});
+sortLastUpBtn.addEventListener(("click"), ()=> {
+    outputList.textContent = "";
+    sortByLastNameUp();
+});
+
+sortLastDownBtn.addEventListener(("click"), ()=> {
+  outputList.textContent = "";
+  sortByLastNameDown();
 });
 
 
@@ -52,7 +68,7 @@ let sortByEducation = async () =>{
   })
 }
 
-let sortByAge = async () =>{
+let sortByAgeUp = async () =>{
      let response = await fetch("https://api.mocki.io/v2/01047e91/students");
      let data = await response.json();
      data.sort((a, b) => a.age - b.age);
@@ -61,7 +77,16 @@ let sortByAge = async () =>{
      })
 }
 
-let sortByFirstName = async () =>{
+let sortByAgeDown = async () =>{
+  let response = await fetch("https://api.mocki.io/v2/01047e91/students");
+  let data = await response.json();
+  data.sort((a, b) => b.age - a.age);
+  data.forEach((person) => {
+     createOutPutCard(person);
+  })
+}
+
+let sortByFirstNameUp = async () =>{
     let response = await fetch("https://api.mocki.io/v2/01047e91/students");
     let data = await response.json();
     data.sort((a, b) => {
@@ -80,7 +105,26 @@ let sortByFirstName = async () =>{
      });
 }
 
-let sortByLastName = async () =>{
+let sortByFirstNameDown = async () =>{
+  let response = await fetch("https://api.mocki.io/v2/01047e91/students");
+  let data = await response.json();
+  data.sort((a, b) => {
+      let nameA = a.firstName.toUpperCase(); 
+      let nameB = b.firstName.toUpperCase(); 
+      if (nameA > nameB) {
+        return -1;
+      }
+      if (nameA < nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  data.forEach((person) => {
+      createOutPutCard(person);
+   });
+}
+
+let sortByLastNameUp = async () =>{
     let response = await fetch("https://api.mocki.io/v2/01047e91/students");
     let data = await response.json();
     data.sort((a, b) => {
@@ -99,35 +143,50 @@ let sortByLastName = async () =>{
      });
 }
 
-let getSchools = async (programme, hobbies) =>{
-  let response = await fetch("https://api.mocki.io/v2/01047e91/schools");
-    let schools = await response.json();
-      // schools.splice(0,1);
-      // schools.splice(2,1);
-      
-      let studentHobbies = hobbies;
-      
-      schools.forEach((school) =>{
-        let programmes = school.programmes;
-        let activities = school.activities;
-       
-     
-
-    if(programmes[0] === programme || programmes[1] === programme){
-           let hasActivity = false;
-            studentHobbies.forEach((hobby) =>{
-              if(activities.includes(hobby)){
-                hasActivity = true;
-                yellow = true;
-                };
-            });
-          if(hasActivity === true){
-            createSchoolCard(school);
-          };
+let sortByLastNameDown = async () =>{
+  let response = await fetch("https://api.mocki.io/v2/01047e91/students");
+  let data = await response.json();
+  data.sort((a, b) => {
+      let nameA = a.lastName.toUpperCase(); 
+      let nameB = b.lastName.toUpperCase(); 
+      if (nameA > nameB) {
+        return -1;
       }
+      if (nameA < nameB) {
+        return 1;
+      }
+      return 0;
     });
-          
+  data.forEach((person) => {
+      createOutPutCard(person);
+   });
 }
+
+// let getSchools = async (programme, hobbies) =>{
+//   let response = await fetch("https://api.mocki.io/v2/01047e91/schools");
+//     let schools = await response.json();
+      
+//       let studentHobbies = hobbies;
+      
+//       schools.forEach((school) =>{
+//         let programmes = school.programmes;
+//         let activities = school.activities;
+      
+//         if(programmes[0] === programme || programmes[1] === programme){
+//            let hasActivity = false;
+//             studentHobbies.forEach((hobby) =>{
+//               if(activities.includes(hobby)){
+//                 hasActivity = true;
+//                 yellow = true;
+//                 };
+//             });
+//           if(hasActivity === true){
+//             createSchoolCard(school);
+//           };
+//       }
+//     });
+          
+// }
       
   
 
@@ -197,34 +256,26 @@ let createOutPutCard = (student) => {
     outputList.appendChild(card);
 }
 
-let createSchoolCard = (school) => {
+// let createSchoolCard = (school) => {
 
-  let card = document.createElement("div")
-    card.id = "card";
-    card.style.background ="white";
+//   let card = document.createElement("div")
+//     card.id = "card";
+//     card.style.background ="white";
 
-    let name = document.createElement("li");
-    name.textContent = "Name: " + school.name;
-    name.style.color ="red";
-    // if(green){
-    //   name.style.color = "green";
-    // }
-    // else if(yellow){
-    //   name.style.color = "yellow";
-    // }
-    // else{
-    //    name.style.color = "red";
-    // }
-    let activities = document.createElement("li");
-    activities.textContent = "Activities: " + school.activities;
-    let programmes = document.createElement("li");
-    programmes.textContent = "Programmes: " + school.programmes;
+//     let name = document.createElement("li");
+//     name.textContent = "Name: " + school.name;
+//     name.style.color ="red";
+  
+//     let activities = document.createElement("li");
+//     activities.textContent = "Activities: " + school.activities;
+//     let programmes = document.createElement("li");
+//     programmes.textContent = "Programmes: " + school.programmes;
 
-    card.appendChild(name);
-    card.appendChild(activities);
-    card.appendChild(programmes);
-    outputList.appendChild(card);
-}
+//     card.appendChild(name);
+//     card.appendChild(activities);
+//     card.appendChild(programmes);
+//     outputList.appendChild(card);
+// }
 
 getStudents()
 //getSchools();
